ebb3b4d78e9dbb1234a5ecf003298a564e0fe416
Anel Hadzic <77800103+AnelHadzic@users.noreply.github.com>
2024-01-08 15:34:11 +0100

    Delete

--- backend/src/main/java/com/hfportal/api/repository/LeagueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hfportal.api.repository;
-
-import com.hfportal.api.model.League;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LeagueRepository extends JpaRepository<League, Integer> {
-}
